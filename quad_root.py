0e06533665d6cdf985049453a4788370e40049bf
prodhanm <mustafizur.prodhan@gmail.com>
2025-04-09 22:07:46 -0400

    after running the debugger, the code is now displaying. Need to resolve the Value Error for math domain error'

--- quad_root.py
+++ quad_root.py
@@ -7,7 +7,6 @@ def quadDisc(a, b, c):
 def quadRootsPos(a, b, c):
     disc = quadDisc(a, b, c) 
     rootPos = (-b + sqrt(disc)) / (2 * a)
-    rootNeg = (-b - sqrt(disc)) / (2 * a)
     return rootPos
 
 def quadRootsNeg(a, b, c):
@@ -15,7 +14,7 @@ def quadRootsNeg(a, b, c):
     rootNeg = (-b - sqrt(disc)) / (2 * a)
     return rootNeg
     
-def main(a, b, c):
+def main():
     a = int(input("Enter the value of a: "))
     b = int(input("Enter the value of b: "))
     c = int(input("Enter the value of c: "))
@@ -25,15 +24,11 @@ def main(a, b, c):
         print("The equation has no real roots.")
         #return None
     elif quadDisc(a, b, c) == 0:
-        print(f"The equation has one real root: {rootPlus}")
+        print(f"The equation has one real root: {rootPlus:.2f}")
         #return roots
     else:
-        print(f"The equation has two real roots: {rootPlus} and {rootMinus}")
+        print(f"The equation has two real roots: {rootPlus:.2f} and {rootMinus:.2f}")
         #return roots
 
 if __name__ == "__main__":
-    main(0, 0, 0)
-    # a = int(input("Enter the value of a: "))
-    # b = int(input("Enter the value of b: "))
-    # c = int(input("Enter the value of c: "))
-    # main(a, b, c)
+    main()
